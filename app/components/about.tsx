0c3475481e19e124bc637f41ee487aaffb24c687
a33peng <ashleypeng16@gmail.com>
2026-03-29 21:42:02 -0400

    init

---
+++ app/components/about.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return ( 
+    <> 
+      <div className="flex min-h-screen"> 
+        <div className="mx-24">
+          <h1 className="font-raleway text-4xl mt-10 mb-10"> Ashley Peng</h1>
+          <p className="font-raleway text-lg "> Hi! I'm currently a Computer Engineering student at the University of Waterloo. I'm passionate about coding, learning and building!</p>
+          <p className="font-raleway text-lg"> In my free time you'll catch me exploring cafes, baking and exploring cities.</p>
+        </div>
+      </div>
+    </> 
+  );
+}
---
+++ app/components/nav.tsx
@@ -0,0 +1,10 @@
+export default function NavBar() { 
+    return ( 
+        <div className="font-raleway font-semibold text-xl ml-100"> 
+            <p> About </p> 
+            <p>
+                <a href="mailto:a33peng@uwaterloo.ca"> Contact</a>  
+            </p>
+        </div> 
+    ); 
+}
