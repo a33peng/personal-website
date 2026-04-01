0c2894c17c9da614c00ecb748b206bc5540522d0
a33peng <ashleypeng16@gmail.com>
2026-03-31 23:52:29 -0400

    hover nav effect

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,10 +1,52 @@
+import React, {CSSProperties} from 'react'; 
+
 export default function NavBar() { 
+    const NavText = ({rotation = 0, timing = 300, children} : { 
+        rotation?: number; 
+        timing?: number; 
+        children: React.ReactNode; 
+    }) => { 
+        const [isHovered, setIsHovered] = React.useState(false); 
+
+        const style: CSSProperties ={ 
+            display: 'inline-block',
+            backfaceVisibility: 'hidden',
+            transform: isHovered ? `rotate(${rotation}deg)` : `rotate(0deg)`,
+            transition: `transform ${timing}ms ease, text-decoration-color ${timing}ms ease`,
+            textDecoration: 'underline',
+            textDecorationColor: isHovered ? '#f7cee0' : 'transparent', 
+            textUnderlineOffset: '5px',
+            cursor: 'pointer',
+        };
+
+        React.useEffect(() => { 
+            if (!isHovered) { 
+                return; 
+            }
+
+            const timeoutId = window.setTimeout(() => { 
+                setIsHovered(false)
+            }, timing); 
+
+            return () => { 
+                window.clearTimeout(timeoutId)
+            };
+        }, [isHovered, timing]); 
+        
+        return ( 
+             <span onMouseEnter={() => setIsHovered(true)} style={style}>
+                {children}
+            </span>
+        );
+    };
+
+
     return ( 
         <div className="font-raleway font-semibold text-xl ml-100"> 
-            <p> About </p> 
-            <p>
-                <a href="mailto:a33peng@uwaterloo.ca"> Contact</a>  
-            </p>
+            <NavText rotation={2}> About </NavText> 
+            <a href="mailto:a33peng@uwaterloo.ca"> 
+                <NavText rotation={-2}> Contact </NavText>
+            </a>  
         </div> 
     ); 
 }
